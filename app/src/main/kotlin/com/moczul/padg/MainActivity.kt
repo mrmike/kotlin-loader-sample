2c331ee7a382016520a688b9a78d1f5dd5b3cc31
Michal Moczulski <moczul@gmail.com>
2014-09-30 22:29:00 +0200

    Added kotlin support

---
+++ app/src/main/kotlin/com/moczul/padg/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.moczul.padg
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+public class MainActivity():Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
